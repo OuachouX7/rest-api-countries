4d0849defd09d17c078ea487ba3ade06c7d19e02
OuachouX7 <ouachou9@gmail.com>
2024-11-11 21:31:04 +0100

    modified some stylesssssssssssssssssss and fixed darkMode problemssssssasssssssss

--- src/App.js
+++ src/App.js
@@ -66,7 +66,7 @@ function App() {
       <div className={"form"}>
         <div className="form-left">
           <svg
-            className="svg"
+            className={dark ? 'svgDark' : "svg"}
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 512 512"
           >
